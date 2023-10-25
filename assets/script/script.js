d2375d6939a15938527352affc44893964e250c8
Omar <omarsegura76300@gmail.com>
2023-10-25 16:17:38 +0200

    Ajout JSON et JS

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+// import results from '../../data/movies.json' assert { type: 'json' };
+// console.log(results);
+
+fetch('data/movies.json')
+    .then(res => res.json())
+    .then(data => {
+        const infosContainer = document.querySelector('#infos');
+
+        for (const movie of data['results']) {
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            card.innerHTML = `
+            <img class="imgMovie" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" alt="${movie.title}">
+                <h2>${movie.title}</h2>
+                <p><i>${movie.release_date}</i></p>
+                <p class= "note">${movie.vote_average*10}%</p>
+            `;
+
+            infosContainer.appendChild(card);
+        }
+    });
